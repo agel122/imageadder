ce2f363a636b3cd642e29b2e5d7e6ed1fea8c17e
agel122 <agel122@yandex.ru>
2021-12-26 19:21:08 +0300

    django case finished (with tests): home (list image), add (add image)

--- imageposter/tests.py
+++ imageposter/tests.py
@@ -26,27 +26,25 @@ class ImagePosterTests(TestCase):
             title='testImage',
             cover=self.create_image_file1(),
         )
+        self.client.post(reverse('add'), {
+            'title': 'testImage1',
+            'cover': self.create_image_file1(),
+        })
 
     def test_image_content(self):
         image = PostedPicture.objects.get(id=1)
         expected_title = f'{image.title}'
         self.assertEqual(expected_title, 'testImage')
 
-    """def test_list_view(self):
+    def test_list_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         self.assertContains(response, 'testImage')
-
-    def test_post_view(self):
-        response = self.client.post(reverse('add'), {
-            'title': 'testImage1',
-            'cover': self.create_image_file(),
-        })
-        # self.assertTemplateUsed(response, 'add.html')
-        self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'testImage1')
-        """
+
+
+
 
 
 
